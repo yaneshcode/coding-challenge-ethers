27d236ab7eed624d393d22e17aaa8610fbd26c56
yanesh <yaneshcode@gmail.com>
2019-03-07 19:09:04 -0500

    get nonce of addr

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ethers = require('ethers');
 
-// const provider = new ethers.getDefaultProvider('kovan');
-//
-// //console.log(provider);
-// const acc = new ethers.Wallet(PRIVATE_KEY, provider);
-// //console.log(acc);
-//
-// const amount = ethers.utils.bigNumberify('1');
-// const gasPrice = ethers.utils.bigNumberify('2000000000');
-// const gasLimit = ethers.utils.bigNumberify('210000');
-// const to = acc.address;
-//
-// const transaction = async () => {
-//   const nonce = await acc.getTransactionCount('pending');
-//
-//   console.log(nonce);
-//
-//   const tx = {
-//     to,
-//     nonce,
-//     gasPrice,
-//     gasLimit
-//   }
-//
-//   console.log (tx);
-//   const raw = await acc.sign(tx);
-//   console.log(raw);
-// }
-
-//transaction();
+const provider = new ethers.getDefaultProvider('kovan');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -44,11 +16,13 @@ router.get('/bn/:number', (request, response) => {
   response.send("String: " + bn.toString() + "\nHex: " + bn.toHexString());
 });
 
-
+// Get nonce of an address
 router.post('/getNonce', (request, response) => {
   let address = request.body.address;
-  console.log(address);
-  response.send(address);
+  provider.getTransactionCount(address).then((transactionCount) => {
+    console.log("Total Transactions Ever Sent: " + transactionCount);
+    response.send("Total Transactions Ever Sent: " + transactionCount);
+  });
 });
 
 module.exports = router;
